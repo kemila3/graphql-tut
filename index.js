889972c5b8a81a0eee9e906116c9b9178775fa41
kemiladev <kemiladabare4@gmail.com>
2025-01-29 11:19:19 +0530

    adding related mapings

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const resolvers = {
         review : (_,args) => db.reviews.find((review) => review.id === args.id),
         game : (_,args) => db.games.find((game) => game.id === args.id),
         author : (_,args) => db.authors.find((author) => author.id === args.id),
+    },
+    Game : {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.game_id === parent.id)
+        }
+    },
+    Review : {
+        author : (parent) => db.authors.find((r) => r.id === parent.author_id ),
+        game : (parent) => db.games.find((r) => r.id === parent.game_id)
+
+    },
+    Author : {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.author_id === parent.id)
+        }
     }
 }
 
--- schema.js
+++ schema.js
@@ -5,18 +5,22 @@ type Game {
     id : ID!
     title : String!
     platform : [String!]!
+    reviews : [Review!]
 }
 
 type Review {
     id : ID!
     rating : Int!
     content : String!
+    author : Author!
+    game : Game!
 }
 
 type Author {
     id :ID!
     name : String!
     verified : Boolean!
+    reviews : [Review!]
 }
 
 type Query {
